ff07b0e845ebf593fa4965be49389b36993ede1e
oauguste <augusteosnac@gmail.com>
2023-06-30 16:58:32 -0400

    shopping cart logic added v1

--- shoppingCart/src/components/ShoppingCart.jsx
+++ shoppingCart/src/components/ShoppingCart.jsx
@@ -1,5 +1,11 @@
+import CartItems from "../features/counter/CartItems";
+
 const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+  return (
+    <div>
+      <CartItems />
+    </div>
+  );
 };
 
 export default ShoppingCart;
